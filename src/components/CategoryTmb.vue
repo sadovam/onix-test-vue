19084b06a0b1e807b61840a329ae2e46649b0a49
Sadov Andrii <sadovam@gmail.com>
2021-06-09 10:29:05 +0300

    Init

---
+++ src/components/CategoryTmb.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'CategoryTmb',
+  props: {
+    title: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+h2 {
+  margin: 40px 0 0;
+  color: #128963;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <CategoryTmb title="Category 1"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import CategoryTmb from '@/components/CategoryTmb.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: {
-    HelloWorld,
+    CategoryTmb,
   },
 });
 </script>
